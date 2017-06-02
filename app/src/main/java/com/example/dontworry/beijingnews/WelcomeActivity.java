365d143e531971d55756543bbbc020f4af4f017f
saygogo <1183769411@qq.com>
2017-06-02 11:17:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dontworry/beijingnews/WelcomeActivity.java
@@ -0,0 +1,61 @@
+package com.example.dontworry.beijingnews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.dontworry.beijingnews.activity.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private RelativeLayout rl_root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        rl_root = (RelativeLayout) findViewById(R.id.rl_root);
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setFillAfter(true);
+        aa.setDuration(2000);
+
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        RotateAnimation ra = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+        set.addAnimation(sa);
+        rl_root.startAnimation(set);
+
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dontworry/beijingnews/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dontworry.beijingnews.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dontworry.beijingnews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
